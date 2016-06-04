9912dedccd965d809df409b1c061af298676520c
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-04 23:20:48 +0800

    修改filter

--- src/main/java/com/the/harbor/commons/dubbo/filter/DubboRequestTrackFilter.java
+++ src/main/java/com/the/harbor/commons/dubbo/filter/DubboRequestTrackFilter.java
@@ -1,10 +1,5 @@
 package com.the.harbor.commons.dubbo.filter;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,90 +16,71 @@ import com.the.harbor.base.exception.SystemException;
 import com.the.harbor.base.vo.Response;
 import com.the.harbor.base.vo.ResponseHeader;
 import com.the.harbor.commons.util.CollectionUtil;
+import com.the.harbor.commons.util.ExceptionUtil;
 import com.the.harbor.commons.util.UUIDUtil;
 
 @Activate(group = { Constants.PROVIDER })
 public class DubboRequestTrackFilter implements Filter {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DubboRequestTrackFilter.class);
+	private static final Logger LOG = LoggerFactory.getLogger(DubboRequestTrackFilter.class);
 
-    @Override
-    public Result invoke(Invoker<?> invoker, Invocation invocation) {
-        String reqSV = invoker.getInterface().getName();
-        String reqMethod = invocation.getMethodName();
-        Object[] requestParams = invocation.getArguments();
-        // 交易序列
-        String tradeSeq = UUIDUtil.genId32();
-        // 打印请求参数明细
-        if (CollectionUtil.isEmpty(requestParams)) {
-            if (LOG.isInfoEnabled()) {
-                LOG.info("TRADE_SEQ:{},请求接口:{},请求方法:{},请求参数:{}", tradeSeq, reqSV, reqMethod, "");
-            }
-        } else {
-            if (LOG.isInfoEnabled()) {
-                LOG.info("TRADE_SEQ:{},请求接口:{},请求方法:{},请求参数:{}", tradeSeq, reqSV, reqMethod,
-                        JSON.toJSONString(requestParams));
-            }
-        }
-        // 执行结果
-        Result result = null;
-        try {
-            if (LOG.isInfoEnabled()) {
-                LOG.info("TRADE_SEQ:{},执行调用服务{}类中的{}方法", tradeSeq, reqSV, reqMethod);
-            }
-            result = invoker.invoke(invocation);
-            if (result.hasException()) {
-                Throwable e = result.getException();
-                if (LOG.isErrorEnabled()) {
-                    LOG.error("TRADE_SEQ:{},调用服务{}类中的{}方法发生异常，原因:{}", tradeSeq, reqSV, reqMethod,
-                            result.getException().getMessage(), result.getException());
-                }
-                if (e instanceof BusinessException) {
-                    Response response = new Response();
-                    response.setResponseHeader(new ResponseHeader(((BusinessException) e)
-                            .getErrorCode(), ((BusinessException) e).getErrorMessage()));
-                    RpcResult r = new RpcResult();
-                    r.setValue(response);
-                    return r;
-                } else if (e instanceof SystemException) {
-                    throw (SystemException) e;
-                } else {
-                    throw new SystemException((Exception) e);
-                }
-            }
-            if (LOG.isInfoEnabled()) {
-                LOG.info("TRADE_SEQ:{},调用服务{}类中的{}方法的结果:{}", tradeSeq, reqSV, reqMethod,
-                        JSON.toJSONString(result.getValue()));
-            }
-            return result;
-        } catch (Exception ex) {
-            if (LOG.isErrorEnabled()) {
-                LOG.error("TRADE_SEQ:{},执行{}类中的{}方法发生异常:{}", tradeSeq, reqSV, reqMethod, ex);
-            }
-            RpcResult r = new RpcResult();
-            if (ex.getCause() instanceof ConstraintViolationException) {
-                if (LOG.isErrorEnabled()) {
-                    LOG.error("TRADE_SEQ:{},执行{}类中的{}方法发生参数约束性校验异常:{},将被转换成业务异常输出", tradeSeq,
-                            reqSV, reqMethod, ex);
-                }
-                ConstraintViolationException ve = (ConstraintViolationException) ex.getCause();
-                Set<ConstraintViolation<?>> violations = ve.getConstraintViolations();
-                if (violations != null && violations.size() > 0) {
-                    String error = null;
-                    for (ConstraintViolation<?> cv : violations) {
-                        error = cv.getMessage();
-                        break;
-                    }
-                    Response response = new Response();
-                    response.setResponseHeader(new ResponseHeader("888888", error));
-                    r.setValue(response);
-                }
-            } else {
-                r.setException(ex);
-            }
-            return r;
-        }
+	@Override
+	public Result invoke(Invoker<?> invoker, Invocation invocation) {
+		String reqSV = invoker.getInterface().getName();
+		String reqMethod = invocation.getMethodName();
+		Object[] requestParams = invocation.getArguments();
+		// 交易序列
+		String tradeSeq = UUIDUtil.genId32();
+		// 打印请求参数明细
+		if (CollectionUtil.isEmpty(requestParams)) {
+			if (LOG.isInfoEnabled()) {
+				LOG.info("TRADE_SEQ:{},请求接口:{},请求方法:{},请求参数:{}", tradeSeq, reqSV, reqMethod, "");
+			}
+		} else {
+			if (LOG.isInfoEnabled()) {
+				LOG.info("TRADE_SEQ:{},请求接口:{},请求方法:{},请求参数:{}", tradeSeq, reqSV, reqMethod,
+						JSON.toJSONString(requestParams));
+			}
+		}
+		// 执行结果
+		Result result = null;
+		try {
+			if (LOG.isInfoEnabled()) {
+				LOG.info("TRADE_SEQ:{},执行调用服务{}类中的{}方法", tradeSeq, reqSV, reqMethod);
+			}
+			result = invoker.invoke(invocation);
+			if (result.hasException()) {
+				Throwable e = result.getException();
+				if (LOG.isErrorEnabled()) {
+					LOG.error("TRADE_SEQ:{},调用服务{}类中的{}方法发生异常，原因:{}", tradeSeq, reqSV, reqMethod,
+							result.getException().getMessage(), result.getException());
+				}
+				if (e instanceof BusinessException) {
+					Response response = new Response();
+					response.setResponseHeader(new ResponseHeader(((BusinessException) e).getErrorCode(),
+							((BusinessException) e).getErrorMessage()));
+					RpcResult r = new RpcResult();
+					r.setValue(response);
+					return r;
+				} else if (e instanceof SystemException) {
+					throw (SystemException) e;
+				} else {
+					throw new SystemException((Exception) e);
+				}
+			}
+			if (LOG.isInfoEnabled()) {
+				LOG.info("TRADE_SEQ:{},调用服务{}类中的{}方法的结果:{}", tradeSeq, reqSV, reqMethod,
+						JSON.toJSONString(result.getValue()));
+			}
+			return result;
+		} catch (Exception ex) {
+			if (LOG.isErrorEnabled()) {
+				LOG.error("TRADE_SEQ:{},执行{}类中的{}方法发生异常:{}", tradeSeq, reqSV, reqMethod, ex);
+			}
+			RpcResult r = ExceptionUtil.convert(ex);
+			return r;
+		}
 
-    }
+	}
 
 }
--- src/main/java/com/the/harbor/commons/util/ExceptionUtil.java
+++ src/main/java/com/the/harbor/commons/util/ExceptionUtil.java
@@ -1,16 +1,73 @@
 package com.the.harbor.commons.util;
 
 import java.io.StringWriter;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+import com.alibaba.dubbo.rpc.RpcResult;
+import com.the.harbor.base.constants.ExceptCodeConstants;
+import com.the.harbor.base.exception.BusinessException;
+import com.the.harbor.base.exception.SystemException;
+import com.the.harbor.base.vo.Response;
+import com.the.harbor.base.vo.ResponseHeader;
+import com.the.harbor.commons.web.model.ResponseData;
 
 public final class ExceptionUtil {
 
-    private ExceptionUtil() {
+	private ExceptionUtil() {
 
-    }
+	}
 
-    public static String getTrace(Throwable t) {
-        StringWriter stringWriter = new StringWriter();
-        return stringWriter.getBuffer().toString();
-    }
+	public static String getTrace(Throwable t) {
+		StringWriter stringWriter = new StringWriter();
+		return stringWriter.getBuffer().toString();
+	}
+
+	public static final <T> ResponseData<T> convert(Exception ex, Class<T> clazz) {
+		ResponseData<T> responseData = null;
+		if (ex instanceof BusinessException) {
+			responseData = new ResponseData<T>(ResponseData.AJAX_STATUS_FAILURE, ex.getMessage());
+		} else if (ex instanceof SystemException) {
+			responseData = new ResponseData<T>(ResponseData.AJAX_STATUS_FAILURE, ex.getMessage());
+		} else {
+			String error = null;
+			if (ex.getCause() instanceof ConstraintViolationException) {
+				ConstraintViolationException ve = (ConstraintViolationException) ex.getCause();
+				Set<ConstraintViolation<?>> violations = ve.getConstraintViolations();
+				if (violations != null && violations.size() > 0) {
+					for (ConstraintViolation<?> cv : violations) {
+						error = cv.getMessage();
+						break;
+					}
+				}
+			}
+			responseData = new ResponseData<T>(ResponseData.AJAX_STATUS_FAILURE,
+					StringUtil.isBlank(error) ? "系统繁忙，请重试" : error);
+		}
+		return responseData;
+	}
+
+	public static final RpcResult convert(Exception ex) {
+		RpcResult r = new RpcResult();
+		if (ex.getCause() instanceof ConstraintViolationException) {
+			ConstraintViolationException ve = (ConstraintViolationException) ex.getCause();
+			Set<ConstraintViolation<?>> violations = ve.getConstraintViolations();
+			if (violations != null && violations.size() > 0) {
+				String error = null;
+				for (ConstraintViolation<?> cv : violations) {
+					error = cv.getMessage();
+					break;
+				}
+				Response response = new Response();
+				response.setResponseHeader(new ResponseHeader(ExceptCodeConstants.VALID_ERROR, error));
+				r.setValue(response);
+			}
+		} else {
+			r.setException(ex);
+		}
+		return r;
+	}
 
 }
