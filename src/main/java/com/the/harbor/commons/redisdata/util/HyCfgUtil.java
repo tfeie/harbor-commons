4bc1394e963c9b7109d2680f48a655bbd093c13d
zhangchao2 <zhangchao2@asiainfo.com>
2016-08-19 15:28:09 +0800

    1

--- src/main/java/com/the/harbor/commons/redisdata/util/HyCfgUtil.java
+++ src/main/java/com/the/harbor/commons/redisdata/util/HyCfgUtil.java
@@ -52,5 +52,109 @@ public class HyCfgUtil {
 		List<String> users = Arrays.asList(value.split(","));
 		return users.contains(userId);
 	}
+	
+	/**
+	 * ONO活动申请成功后通知海牛的短信模板编码
+	 * @return
+	 */
+	public static String getSMSCodeOfOnOApplied() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_ono_applied");
+		return value;
+	}
+	
+	/**
+	 * ONO活动审核结果通知小白
+	 * @return
+	 */
+	public static String getSMSCodeOfOnOAuditResult() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_ono_audit_result");
+		return value;
+	}
 
+	/**
+	 * ONO活动海牛设置了见面时间通知小白
+	 * @return
+	 */
+	public static String getSMSCodeOfOnOHNSetMeetingInfo() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_ono_hn_set_meeting");
+		return value;
+	}
+	
+	/**
+	 * ONO活动小白选择约见时间信息
+	 * @return
+	 */
+	public static String getSMSCodeOfOnOXBChooseMeetingInfo() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_ono_xb_choose_meeting");
+		return value;
+	}
+	
+	
+	
+	/**
+	 * ONO活动小白评论后通知海牛
+	 * @return
+	 */
+	public static String getSMSCodeOfOnOXBComment() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_ono_xb_comment");
+		return value;
+	}
+	
+	/**
+	 * ONO活动海牛确认服务结束后通知小白
+	 * @return
+	 */
+	public static String getSMSCodeOfOnOHNEnd() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_ono_hn_end");
+		return value;
+	}
+	
+	
+	
+	/**
+	 * 新用户报名参加GROUP通知发起者
+	 * @return
+	 */
+	public static String getSMSCodeOfGroupUserJoin() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_group_user_join");
+		return value;
+	}
+	
+	/**
+	 * 参与者评价GROUP后通知发起者
+	 * @return
+	 */
+	public static String getSMSCodeOfGroupUserComments() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_group_user_comment");
+		return value;
+	}
+
+	/**
+	 * 活动结束3小时后提醒参与者去点评活动
+	 * @return
+	 */
+	public static String getSMSCodeOfGroupRemindUsertoComments() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_group_remind_user_comment");
+		return value;
+	}
+	
+	/**
+	 * GROUP活动审核结果通知参与者
+	 * @return
+	 */
+	public static String getSMSCodeOfGroupAuditResult() {
+		ICacheClient cacheClient = CacheFactory.getClient();
+		String value = cacheClient.hget(RedisDataKey.KEY_CFG.getKey(), "sms_code_group_audit_result");
+		return value;
+	}
+	
 }
